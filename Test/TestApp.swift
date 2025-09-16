2cc9855face3ed1e5b0f06d8b8f6d3b5fd430929
Tonyparker1415 <124949849+Tonyparker1415@users.noreply.github.com>
2025-09-16 09:55:14 -0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Tony Parker on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestTests/TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by Tony Parker on 9/16/25.
+//
+
+import Testing
+@testable import Test
+
+struct TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
